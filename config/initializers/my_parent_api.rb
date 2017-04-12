8f595f12cf4355c863ef6d10d41c01671574519c
twinlamp <vyboyshchik@gmail.com>
2017-04-12 12:58:11 +0000

    yml & initializer

---
+++ config/initializers/my_parent_api.rb
@@ -0,0 +1,31 @@
+require 'httparty'
+apis = YAML.load_file('apis.yml')
+apis.each do |api|
+  klass = Class.new(api['parent_class'].constantize) do
+    define_singleton_method :run do
+      times_retried = 0
+      byebug
+      begin
+        resp = HTTParty.get(api['url'], timeout: api['max_timeout'])&.parsed_response
+      rescue Net::ReadTimeout => error
+        if times_retried < api['max_retries']
+          times_retried += 1
+          puts "Failed to get #{api['url']}, retry #{times_retried}/#{api['max_retries']}"
+          retry
+        else
+          puts "Failed to get #{api['url']} in #{api['max_retries']} attempts"
+          next
+        end
+      end
+      if resp
+        instance = api['parent_class'].constantize.new
+        api['parameter_map'].each do |k,v|
+          instance.send("#{v}=", resp[k])
+        end
+        instance.save
+      end
+    end
+  end
+
+  Object.const_set(api['api_class'], klass)
+end
